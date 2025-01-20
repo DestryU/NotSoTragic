1f4f8e0e9b25c375f4c6a6741c4a651d13741317
DestryU <47061782+DestryU@users.noreply.github.com>
2025-01-20 14:22:49 -0700

    Updated CORS rules to allow hosted frontend communications at https://notsotragic.vercel.app/

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -13,7 +13,7 @@ const app = express()
 const PORT = process.env.PORT
 
 app.use(express.json())
-app.use(cors({ origin: 'http://localhost:3000'}))
+app.use(cors({ origin: 'https://notsotragic.vercel.app/'}))
 
 
 /* Couples a URL with a designated router file */
