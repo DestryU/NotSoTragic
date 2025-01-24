daf4d74865c35b92f1bc8c5b816aad6e2ce423b3
DestryU <47061782+DestryU@users.noreply.github.com>
2025-01-24 14:13:40 -0700

    Reverted axios instance to correctly point to public backend URL.

--- packages/frontend/utils/axiosInstance.ts
+++ packages/frontend/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios'
 
 const axiosInstance: AxiosInstance = axios.create({
-    baseURL: process.env.NEXT_LOCAL_BACKEND_URL || 'http://localhost:5000',
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
